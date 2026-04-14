f5c402658016003cc21264646c7f3e38cb0460b6
bkeithinstep-stack <bkeith.instep@gmail.com>
2026-04-15 01:51:58 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
-  images: {5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -40,36 +23,16 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-
-    domains: ['financialmodelingprep.com', 'twelvedata.com'],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/fmp/:path*',
-        destination: 'https://financialmodelingprep.com/api/:path*',
-      },
-    ];
-  },
-};
-
 module.exports = nextConfig;
+
+
+
+
+
+
+
+
+
 
 
 
