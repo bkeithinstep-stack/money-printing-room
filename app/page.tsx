c08b2e40cf8a3ccbb6395e5bb177d8e81389da60
bkeithinstep-stack <bkeith.instep@gmail.com>
2026-04-15 04:01:52 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
             Welcome{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
-            </span>span>
+            </span>span→
           </h2>h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Start building your amazing project.
