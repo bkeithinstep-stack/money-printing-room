64347432829a664049dd242a608b26ab365eb393
bkeithinstep-stack <bkeith.instep@gmail.com>
2026-04-15 00:48:44 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
+  swcM
+  images: {5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+
     domains: ['financialmodelingprep.com', 'twelvedata.com'],
   },
   async rewrites() {
@@ -16,7 +46,6 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
 
 
 
